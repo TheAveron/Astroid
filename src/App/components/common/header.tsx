e20459949b2a25fa555544570de53504ebb292cd
Averon <77922089+TheAveron@users.noreply.github.com>
2023-10-22 15:07:08 +0200

    fix: tweaking some things

--- src/App/components/common/header.tsx
+++ src/App/components/common/header.tsx
@@ -5,7 +5,7 @@ import ThemeChoice from "../../utils/theme"
 function Header() {
     return (
         <header>
-            <div className="title"><Link to={"/"}>Astroid</Link></div>
+            <div className="title"><Link to={"/Astroid"}>Astroid</Link></div>
             <nav className="navbar"><Link to={"/play"}>Play</Link><ThemeChoice /></nav>
         </header>
     )
